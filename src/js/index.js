08373ab056429cace95e2b3a4bde01024cc4dea4
Svitlana Vasylkovska <vaselisssa@gmail.com>
2023-08-13 22:37:13 +0300

    correct js

--- src/js/index.js
+++ src/js/index.js
@@ -7,69 +7,71 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 axios.defaults.headers.common['x-api-key'] =
   'live_KEFcsZ7BwwxdtSthiDwqiQOso8Ovv3cgwICUAJZYIisvnSedi7wG2cqHzZ00shvL';
 
-const selectCat = document.querySelector('.breed-select');
-const catInfo = document.querySelector('.cat-info');
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
+const refs = {
+  breedSelect: document.querySelector('.breed-select'),
+  catInfo: document.querySelector('.cat-info'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+};
 
-selectCat.addEventListener('change', onSelectChange);
+refs.breedSelect.addEventListener('change', onSelectChange);
 
-function createCatList() {
-  // Показуємо лоадер перед початком запиту
-  loader.classList.remove('is-hidden');
-  selectCat.classList.add('is-hidden');
-  error.classList.add('is-hidden');
+function onSelectChange(e) {
+  refs.loader.hidden = false;
+  refs.breedSelect.hidden = true;
 
-  //обробляємо результат запиту на бекенд (всі породи котів)
+  refs.catInfo.innerHTML = '';
+
+  const breedId = e.currentTarget.value;
+
+  fetchCatByBreed(breedId)
+    .then(data => {
+      refs.catInfo.innerHTML = createMarkup(data);
+    })
+    .catch(error => {
+      Notify.failure('Oops! Something went wrong! Try reloading the page!');
+    })
+    .finally(() => {
+      refs.loader.hidden = true;
+      refs.breedSelect.hidden = false;
+      refs.catInfo.hidden = false;
+    });
+}
+
+function createOptions() {
+  refs.breedSelect.hidden = true;
+
   fetchBreeds()
     .then(data => {
-      const optionsList = data
+      const options = data
         .map(({ id, name }) => ` <option value="${id}">${name}</option>`)
-        .join(' ');
+        .join('');
 
-      selectCat.innerHTML = optionsList;
+      refs.breedSelect.innerHTML = options;
 
-      //стилізуємо селект з доп бібліотеки SlimSelect
       new SlimSelect({
-        select: selectCat,
+        select: refs.breedSelect,
       });
-      // Отримали дані успішно, ховаємо лоадер показуємо селект
-      loader.classList.add('is-hidden');
-      selectCat.classList.remove('is-hidden');
     })
     .catch(error => {
       Notify.failure('Oops! Something went wrong! Try reloading the page!');
-    });
-}
-
-createCatList();
-
-function onSelectChange(evt) {
-  loader.classList.remove('is-hidden');
-  catInfo.classList.add('is-hidden');
-
-  const selectedBreedId = evt.currentTarget.value;
-
-  fetchCatByBreed(selectedBreedId)
-    .then(data => {
-      renderMarkupInfo(data);
-      loader.classList.add('is-hidden');
-      catInfo.classList.remove('is-hidden');
     })
-    .catch(error => {
-      loader.classList.add('is-hidden');
-      Notify.failure('Oops! Something went wrong! Try reloading the page!');
+    .finally(() => {
+      refs.error.hidden = true;
+      refs.loader.hidden = true;
+      refs.breedSelect.hidden = false;
     });
 }
+createOptions();
 
-function renderMarkupInfo(data) {
-  const { breeds, url } = data[0];
-  const { name, temperament, description } = breeds[0];
-  const beerdCard = `<img class="pfoto-cat" width = "300px" src="${url}" alt="${name}">
-    <div class="text-part">
-  <h2 class="name-cat">${name}</h2>
-  <p class="deskr-cat">${description}</p>
-  <p class="temperament-cat"><span class="temperament-label">Temperament:</span> ${temperament}</p>  </div>`;
-
-  catInfo.innerHTML = beerdCard;
+function createMarkup(array) {
+  return array
+    .map(({ url, breeds: [{ description, name, temperament }] }) => {
+      return `<img class="pfoto-cat" width = "300px" src="${url}" alt="${name}">
+      <div class="text-part">
+      <h2 class="name-cat">${name}</h2>
+      <p class="deskr-cat">${description}</p>
+      <p class="temperament-cat"><span class="temperament-label">Temperament:</span> ${temperament}</p>  </div>`;
+    })
+    .join('');
 }
