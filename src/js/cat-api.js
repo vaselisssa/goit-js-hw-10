458b5c779fdc8121c0b7df5a9d1ee87b53e334d0
Svitlana Vasylkovska <vaselisssa@gmail.com>
2023-08-14 04:34:02 +0300

    fix

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
+axios.defaults.headers.common['x-api-key'] =
+  'live_KEFcsZ7BwwxdtSthiDwqiQOso8Ovv3cgwICUAJZYIisvnSedi7wG2cqHzZ00shvL';
+
 const BASE_URL = 'https://api.thecatapi.com/v1';
 
 function fetchBreeds() {
   return axios
     .get(`${BASE_URL}/breeds`)
-    .then(resp => {
-      return resp.data;
-    })
+    .then(resp => resp.data)
     .catch(error => {
       throw new Error('Помилка запиту:', error.message);
     });
@@ -16,9 +17,7 @@ function fetchBreeds() {
 function fetchCatByBreed(breedId) {
   return axios
     .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
-    .then(resp => {
-      return resp.data;
-    })
+    .then(resp => resp.data)
     .catch(error => {
       throw new Error('Помилка запиту:', error.message);
     });
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,7 @@
-import axios from 'axios';
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-axios.defaults.headers.common['x-api-key'] =
-  'live_KEFcsZ7BwwxdtSthiDwqiQOso8Ovv3cgwICUAJZYIisvnSedi7wG2cqHzZ00shvL';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const refs = {
   breedSelect: document.querySelector('.breed-select'),
@@ -14,64 +10,76 @@ const refs = {
   error: document.querySelector('.error'),
 };
 
+hideElement(refs.error);
+hideElement(refs.breedSelect);
+
+fetchBreeds()
+  .then(breeds => {
+    const options = breeds
+      .map(({ id, name }) => ` <option value="${id}">${name}</option>`)
+      .join('');
+    refs.breedSelect.innerHTML = options;
+
+    new SlimSelect({
+      select: '.breed-select',
+    });
+
+    hideElement(refs.loader);
+
+    showElement(refs.breedSelect);
+  })
+  .catch(error => {
+    console.log(error);
+
+    hideElement(refs.loader);
+    Notify.failure(refs.error.textContent);
+  });
+
 refs.breedSelect.addEventListener('change', onSelectChange);
 
-function onSelectChange(e) {
-  refs.loader.hidden = false;
-  refs.breedSelect.hidden = true;
-
+function onSelectChange() {
+  const breedId = refs.breedSelect.value;
   refs.catInfo.innerHTML = '';
-
-  const breedId = e.currentTarget.value;
-
+  showElement(refs.loader);
   fetchCatByBreed(breedId)
-    .then(data => {
-      refs.catInfo.innerHTML = createMarkup(data);
+    .then(catData => {
+      createMarkup(catData);
     })
     .catch(error => {
-      Notify.failure('Oops! Something went wrong! Try reloading the page!');
-    })
-    .finally(() => {
-      refs.loader.hidden = true;
-      refs.breedSelect.hidden = false;
-      refs.catInfo.hidden = false;
+      console.log(error);
+      hideElement(refs.loader);
+
+      Notify.failure(refs.error.textContent);
     });
 }
-
-function createOptions() {
-  refs.breedSelect.hidden = true;
-
-  fetchBreeds()
-    .then(data => {
-      const options = data
-        .map(({ id, name }) => ` <option value="${id}">${name}</option>`)
-        .join('');
-
-      refs.breedSelect.innerHTML = options;
-
-      new SlimSelect({
-        select: refs.breedSelect,
-      });
-    })
-    .catch(error => {
-      Notify.failure('Oops! Something went wrong! Try reloading the page!');
-    })
-    .finally(() => {
-      refs.error.hidden = true;
-      refs.loader.hidden = true;
-      refs.breedSelect.hidden = false;
-    });
-}
-createOptions();
 
 function createMarkup(array) {
-  return array
-    .map(({ url, breeds: [{ description, name, temperament }] }) => {
-      return `<img class="pfoto-cat" width = "300px" src="${url}" alt="${name}">
-      <div class="text-part">
-      <h2 class="name-cat">${name}</h2>
-      <p class="deskr-cat">${description}</p>
-      <p class="temperament-cat"><span class="temperament-label">Temperament:</span> ${temperament}</p>  </div>`;
-    })
+  console.dir(array);
+  const imgUrl = array[0].url;
+  const arrBreeds = array[0].breeds[0];
+  const title = arrBreeds.name;
+  const description = arrBreeds.description;
+  const temperament = arrBreeds.temperament;
+
+  hideElement(refs.loader);
+  const catCard = array
+
+    .map(
+      elem =>
+        `<img class="pfoto-cat" width = "300px" src="${imgUrl}" alt="cat ${title}">
+        <div>
+        <h2>${title}</h2>
+        <p>${description}</p>
+        <p> <span class="temperament-label">Temperament: </span>${temperament}</p></div>`
+    )
     .join('');
+  refs.catInfo.insertAdjacentHTML('beforeend', catCard);
+}
+
+function showElement(element) {
+  element.classList.remove('is-hidden');
+}
+
+function hideElement(element) {
+  element.classList.add('is-hidden');
 }
